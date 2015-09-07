86e88ddcdd53c3d3f26e67b57090675aa96e3e7c
Aman Mangal <mangalaman93@gmail.com>
2015-09-07 16:48:48 -0400

    nf : update sipp to compute ip address

--- nf/sipp/run_sipp.sh
+++ nf/sipp/run_sipp.sh
@@ -4,5 +4,9 @@ if [ -z "$ARGS" ]; then
     exit 1
 fi
 
-cd /data && sipp -bg -trace_stat -fd 1s -trace_rtt -rtt_freq 1 $ARGS
+# wait for networking to setup
+sleep 5
+
+IF=$(ifconfig | grep "inet " | awk -F'[: ]+' '{print $4}')
+cd /data && sipp -bg -trace_stat -fd 1s -trace_rtt -rtt_freq 1 -trace_logs -trace_err -i $IF $ARGS
 tail -f /dev/null
