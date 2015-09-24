0232983befec25e052b0d37e0986fec78d5cf9f8
Aman Mangal <mangalaman93@gmail.com>
2015-09-24 19:36:19 -0400

    general : update scripts to create multiple client-servers

--- scripts/voip.sh
+++ scripts/voip.sh
@@ -36,7 +36,7 @@ stop_exp() {
   sudo kill $(pidof monsipp) &>/dev/null
 
   echo "delete containers"
-  nova delete sipp-server sipp-client snort &>/dev/null
+  nova delete sipp-server1 sipp-server2 sipp-client1 sipp-client2 snort &>/dev/null
 
   echo "delete volumes"
   sudo rm -rf /var/lib/docker/volumes/*
@@ -49,6 +49,25 @@ check_host() {
   THIS_HOST=$(nova show $1 | grep "OS-EXT-SRV-ATTR:hypervisor_hostname" | awk '{print $4}')
   if [[ $THIS_HOST != $2 ]]; then
     echo "Error: $1 started on unexpected host $THIS_HOST instead of $2!"
+    stop_exp
+    exit 1
+  fi
+}
+
+# $1 -> id
+check_show() {
+  nova show $1 &>/dev/null
+  if [[ $? -eq 0 ]]; then
+    echo "Error: $1 is already running!"
+    exit 1
+  fi
+}
+
+# $1 -> id
+check_show_running() {
+  nova show $1 &>/dev/null
+  if [[ $? -ne 0 ]]; then
+    echo "Error: $1 did not run!"
     stop_exp
     exit 1
   fi
@@ -67,23 +86,11 @@ start_exp_on_cur() {
     exit 1
   fi
 
-  nova show sipp-server &>/dev/null
-  if [[ $? -eq 0 ]]; then
-    echo "Error: sipp-server is already running!"
-    exit 1
-  fi
-
-  nova show sipp-client &>/dev/null
-  if [[ $? -eq 0 ]]; then
-    echo "Error: sipp-client is already running!"
-    exit 1
-  fi
-
-  nova show snort &>/dev/null
-  if [[ $? -eq 0 ]]; then
-    echo "Error: snort is already running!"
-    exit 1
-  fi
+  check_show sipp-server1
+  check_show sipp-server2
+  check_show sipp-client1
+  check_show sipp-client2
+  check_show snort
 
   # run cadvisor
   docker run -d --net=host --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro --name=$CUR_HOST-cadvisor mangalaman93/cadvisor -storage_driver=influxdb -storage_driver_user=$INFLUXDB_USER -storage_driver_password=$INFLUXDB_PASS -storage_driver_host=$INFLUXDB_IP:$INFLUXDB_PORT > /dev/null
@@ -91,14 +98,23 @@ start_exp_on_cur() {
   # run monsipp
   cd ~/nfs/ && sudo ./monsipp -d $INFLUXDB_IP:$INFLUXDB_PORT:$INFLUXDB_USER:$INFLUXDB_PASS && cd
 
-  # run sipp server
-  echo "running sipp-server"
-  nova boot --image mangalaman93/sipp --meta ARGS="-sn uas" --availability-zone regionOne:$CUR_HOST --flavor c1.medium sipp-server > /dev/null
+  # run sipp server1
+  echo "running sipp-server1"
+  nova boot --image mangalaman93/sipp --meta ARGS="-sn uas" --availability-zone regionOne:$CUR_HOST --flavor c1.medium sipp-server1 > /dev/null
   sleep 3
-  check_host sipp-server $CUR_HOST
-  find_ip sipp-server
-  SERVER_IP=$OUT_IP
-  echo "server: $SERVER_IP"
+  check_host sipp-server1 $CUR_HOST
+  find_ip sipp-server1
+  SERVER1_IP=$OUT_IP
+  echo "server1: $SERVER1_IP"
+
+  # run sipp server2
+  echo "running sipp-server2"
+  nova boot --image mangalaman93/sipp --meta ARGS="-sn uas" --availability-zone regionOne:$CUR_HOST --flavor c1.medium sipp-server2 > /dev/null
+  sleep 3
+  check_host sipp-server2 $CUR_HOST
+  find_ip sipp-server2
+  SERVER2_IP=$OUT_IP
+  echo "server2: $SERVER2_IP"
 
   # run snort
   echo "running snort"
@@ -109,22 +125,37 @@ start_exp_on_cur() {
   ROUTER_IP=$OUT_IP
   echo "router: $ROUTER_IP"
 
-  # run sipp-client
-  echo "running sipp-client"
-  nova boot --image mangalaman93/sipp --meta ARGS="-sn uac $SERVER_IP:5060" --availability-zone regionOne:$CUR_HOST --flavor c1.medium sipp-client > /dev/null
+  # run sipp-client1
+  echo "running sipp-client1"
+  nova boot --image mangalaman93/sipp --meta ARGS="-sn uac $SERVER1_IP:5060" --availability-zone regionOne:$CUR_HOST --flavor c1.medium sipp-client1 > /dev/null
   sleep 3
-  check_host sipp-client $CUR_HOST
-  CLIENT_ID=$(nova show sipp-client | grep "| id" | awk '{print $4}')
-  echo "client-id: $CLIENT_ID"
-  find_ip sipp-client
-  CLIENT_IP=$OUT_IP
-  echo "client: $CLIENT_IP"
+  check_host sipp-client1 $CUR_HOST
+  CLIENT1_ID=$(nova show sipp-client1 | grep "| id" | awk '{print $4}')
+  echo "client1-id: $CLIENT1_ID"
+  find_ip sipp-client1
+  CLIENT1_IP=$OUT_IP
+  echo "client1: $CLIENT1_IP"
+
+  # run sipp-client2
+  echo "running sipp-client2"
+  nova boot --image mangalaman93/sipp --meta ARGS="-sn uac $SERVER2_IP:5060" --availability-zone regionOne:$CUR_HOST --flavor c1.medium sipp-client2 > /dev/null
+  sleep 3
+  check_host sipp-client2 $CUR_HOST
+  CLIENT2_ID=$(nova show sipp-client2 | grep "| id" | awk '{print $4}')
+  echo "client2-id: $CLIENT2_ID"
+  find_ip sipp-client2
+  CLIENT2_IP=$OUT_IP
+  echo "client2: $CLIENT2_IP"
 
   # settings
   echo "setting up routes"
-  FULL_CLIENT_DOCKER_ID=$(docker inspect --format '{{ .Id }}' nova-$CLIENT_ID)
-  echo "docker-client-id: $FULL_CLIENT_DOCKER_ID"
-  sudo ip netns exec $FULL_CLIENT_DOCKER_ID ip route add $SERVER_IP via $ROUTER_IP
+  FULL_CLIENT1_DOCKER_ID=$(docker inspect --format '{{ .Id }}' sipp-client1-$CLIENT1_ID)
+  echo "docker-client1-id: $FULL_CLIENT1_DOCKER_ID"
+  sudo ip netns exec $FULL_CLIENT1_DOCKER_ID ip route add $SERVER1_IP via $ROUTER_IP
+
+  FULL_CLIENT2_DOCKER_ID=$(docker inspect --format '{{ .Id }}' sipp-client2-$CLIENT2_ID)
+  echo "docker-client2-id: $FULL_CLIENT2_DOCKER_ID"
+  sudo ip netns exec $FULL_CLIENT2_DOCKER_ID ip route add $SERVER2_IP via $ROUTER_IP
 
   echo "running checks..."
   docker inspect $CUR_HOST-cadvisor &>/dev/null
@@ -137,20 +168,11 @@ start_exp_on_cur() {
     echo "Error: monsipp did not run!"
   fi
 
-  nova show sipp-server &>/dev/null
-  if [[ $? -ne 0 ]]; then
-    echo "Error: sipp-server did not run!"
-  fi
-
-  nova show sipp-client &>/dev/null
-  if [[ $? -ne 0 ]]; then
-    echo "Error: sipp-client did not run!"
-  fi
-
-  nova show snort &>/dev/null
-  if [[ $? -ne 0 ]]; then
-    echo "Error: snort did not run!"
-  fi
+  check_show_running sipp-server1
+  check_show_running sipp-server2
+  check_show_running sipp-client1
+  check_show_running sipp-client2
+  check_show_running snort
 }
 
 start_exp_on_oth() {
@@ -166,27 +188,15 @@ start_exp_on_oth() {
     exit 1
   fi
 
-  nova show sipp-server &>/dev/null
-  if [[ $? -ne 0 ]]; then
-    echo "Error: sipp-server is not running!"
-    exit 1
-  fi
-
-  nova show sipp-client &>/dev/null
-  if [[ $? -ne 0 ]]; then
-    echo "Error: sipp-client is not running!"
-    exit 1
-  fi
-
-  nova show snort &>/dev/null
-  if [[ $? -ne 0 ]]; then
-    echo "Error: snort is not running!"
-    exit 1
-  fi
+  check_show_running sipp-server1
+  check_show_running sipp-server2
+  check_show_running sipp-client1
+  check_show_running sipp-client2
+  check_show_running snort
 
   SNORT_ID=$(nova show snort | grep "| id" | awk '{print $4}')
   echo "snort-id: $SNORT_ID"
-  docker inspect nova-$SNORT_ID &>/dev/null
+  docker inspect snort-$SNORT_ID &>/dev/null
   if [[ $? -ne 0 ]]; then
     echo "Error: snort is not running on $OTH_HOST"
     exit 1
@@ -198,21 +208,31 @@ start_exp_on_oth() {
   # run monsipp
   cd ~/nfs/ && sudo ./monsipp -d $INFLUXDB_IP:$INFLUXDB_PORT:$INFLUXDB_USER:$INFLUXDB_PASS && cd
 
-  find_ip sipp-server
-  SERVER_IP=$OUT_IP
-  echo "server: $SERVER_IP"
-  find_ip sipp-client
-  CLIENT_IP=$OUT_IP
-  echo "client: $CLIENT_IP"
+  find_ip sipp-server1
+  SERVER1_IP=$OUT_IP
+  echo "server1: $SERVER1_IP"
+  find_ip sipp-client1
+  CLIENT1_IP=$OUT_IP
+  echo "client1: $CLIENT1_IP"
+  find_ip sipp-server2
+  SERVER2_IP=$OUT_IP
+  echo "server2: $SERVER2_IP"
+  find_ip sipp-client2
+  CLIENT2_IP=$OUT_IP
+  echo "client2: $CLIENT2_IP"
   find_ip snort
   ROUTER_IP=$OUT_IP
   echo "router: $ROUTER_IP"
 
-  FULL_SNORT_DOCKER_ID=$(docker inspect --format '{{ .Id }}' nova-$SNORT_ID)
+  FULL_SNORT_DOCKER_ID=$(docker inspect --format '{{ .Id }}' snort-$SNORT_ID)
   echo "snort-docker-id: $FULL_SNORT_DOCKER_ID"
-  sudo ip netns exec $FULL_SNORT_DOCKER_ID iptables -t nat -A PREROUTING -d $ROUTER_IP -s $CLIENT_IP -j DNAT --to-destination $SERVER_IP
-  sudo ip netns exec $FULL_SNORT_DOCKER_ID iptables -t nat -I POSTROUTING -s $CLIENT_IP -j SNAT --to-source $ROUTER_IP
-  sudo ip netns exec $FULL_SNORT_DOCKER_ID iptables -A FORWARD -d $SERVER_IP -i eth0 -j ACCEPT
+  sudo ip netns exec $FULL_SNORT_DOCKER_ID iptables -t nat -A PREROUTING -d $ROUTER_IP -s $CLIENT1_IP -j DNAT --to-destination $SERVER1_IP
+  sudo ip netns exec $FULL_SNORT_DOCKER_ID iptables -t nat -I POSTROUTING -s $CLIENT1_IP -j SNAT --to-source $ROUTER_IP
+  sudo ip netns exec $FULL_SNORT_DOCKER_ID iptables -A FORWARD -d $SERVER1_IP -i eth0 -j ACCEPT
+
+  sudo ip netns exec $FULL_SNORT_DOCKER_ID iptables -t nat -A PREROUTING -d $ROUTER_IP -s $CLIENT2_IP -j DNAT --to-destination $SERVER2_IP
+  sudo ip netns exec $FULL_SNORT_DOCKER_ID iptables -t nat -I POSTROUTING -s $CLIENT2_IP -j SNAT --to-source $ROUTER_IP
+  sudo ip netns exec $FULL_SNORT_DOCKER_ID iptables -A FORWARD -d $SERVER2_IP -i eth0 -j ACCEPT
 
   echo "running checks..."
   docker inspect $OTH_HOST-cadvisor &>/dev/null
