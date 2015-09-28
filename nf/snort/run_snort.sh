1efdc4398cf0ba2c0fbe19d10797b0beaae4807d
Aman Mangal <mangalaman93@gmail.com>
2015-09-27 23:13:42 -0400

    nf : run snort in daemon mode

--- nf/snort/run_snort.sh
+++ nf/snort/run_snort.sh
@@ -4,4 +4,4 @@ export LUA_PATH=$snort_path/include/snort/lua/\?.lua\;\;
 export SNORT_LUA_PATH=$snort_path/etc/snort
 
 iptables -A FORWARD -j NFQUEUE
-snort -c $snort_path/etc/snort/snort.lua -R $snort_path/etc/snort/sample.rules --max-packet-threads 8 --daq nfq -Q -L dump -l /log/
+snort -c $snort_path/etc/snort/snort.lua -R $snort_path/etc/snort/sample.rules --max-packet-threads 8 --daq nfq -Q -L dump -l /log/ -D
