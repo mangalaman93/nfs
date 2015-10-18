dafd38f5bd98d540d847a2678be21f0fb9346a43
Aman Mangal <mangalaman93@gmail.com>
2015-10-18 08:57:41 -0400

    control : use old pyretic repository, update policy

--- control/README.md
+++ control/README.md
@@ -1,9 +1,9 @@
 # build pyretic container
 ```
-docker build --rm -t mangalaman93/pyretic routing/
+docker build --rm -t mangalaman93/pyretic control/
 ```
 
 # Run Pyretic
 ```
-docker run --rm -it -p 6633:6633 mangalaman93/pyretic pyretic.examples.policy [SERVER_IPs]
+docker run --rm -it -p 6633:6633 mangalaman93/pyretic pyretic.examples.policy [snort mac] [SERVER_IPs]
 ```
--- control/policy.py
+++ control/policy.py
@@ -5,11 +5,17 @@ from pyretic.lib.corelib import *
 from pyretic.lib.std import *
 
 def main():
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 4:
+    	print "usage: <mac-snort> <ip-servers>"
         raise "Need server IPs as arguments"
-    servers = sys.argv[2:]
+    snort_mac = sys.argv[2]
+    servers = sys.argv[3:]
 
     policy = None
     for s in servers:
-        policy = policy + (match(dstip=s) >> fwd())
+    	m = (match(dstip=IPAddr(s)) >> modify(dstmac=EthAddr(snort_mac)))
+    	if policy:
+        	policy = policy + m
+        else:
+        	policy = m
     return policy
