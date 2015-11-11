de5d13ee20f6062db3eff9020218972724dc324e
Aman Mangal <mangalaman93@gmail.com>
2015-11-10 20:35:27 -0500

    scripts : update rtt script

--- scripts/rtt_stats.rb
+++ scripts/rtt_stats.rb
@@ -3,35 +3,31 @@
 require 'csv'
 
 unless ARGV.length == 1
-  puts "usage: ruby send_rt.rb <folder>"
+  puts "usage: ruby rtt_stats.rb <file>"
   exit
 end
 
-title = "server"
-for exp in 1..3
-  for j in 0..(exp-1)
-    file = File.join(ARGV[0], "#{exp}", "ua#{title[0]}#{j}_10_rtt.csv")
-    puts "reading file #{file}"
-    other_count = 0
-    count = 0
-    sum = 0
-    sums = 0
-    CSV.foreach(file, {:col_sep => ";"}) do |row|
-      if count == 0
-        count += 1
-        next
-      end
-      num = row[1].to_f
-      if num > 1000
-        other_count += 1
-        next
-      end
-      count += 1
-      sum += num
-      sums += (num*num)
-    end
-    count -= 1
-    puts "sum:#{sum} sums:#{sums} count:#{count} other_count:#{other_count}"
-    puts "mean:#{sum/count}, std:#{Math.sqrt(sums/count - (sum/count)*(sum/count))}"
+file = ARGV[0]
+puts "reading file #{file}"
+other_count = 0
+count = 0
+sum = 0
+sums = 0
+CSV.foreach(file, {:col_sep => ";"}) do |row|
+  if count == 0
+    count += 1
+    next
   end
+  num = row[1].to_f
+  if num > 1000
+    other_count += 1
+    next
+  end
+  count += 1
+  sum += num
+  sums += (num*num)
 end
+
+count -= 1
+puts "sum:#{sum} sums:#{sums} count:#{count} other_count:#{other_count}"
+puts "mean:#{sum/count}, std:#{Math.sqrt(sums/count - (sum/count)*(sum/count))}"
